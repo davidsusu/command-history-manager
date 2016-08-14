b09fdd89fe86deb081f39ac0b26e12793abacbe9
susu <susu@asztali.otthon>
2016-08-14 09:32:02 +0200

    ComplexHistory added (not implemented yet)

---
+++ command-history-manager/src/hu/webarticum/chm/ComplexHistory.java
@@ -0,0 +1,15 @@
+package hu.webarticum.chm;
+
+public class ComplexHistory {
+	
+	// TODO: maximumCapacity
+	
+	// TODO: class Tree
+	//        - size (int, cached information about full size)
+	//        - subTrees (LinkedList)
+	//        - defaultSubTree (Tree, reference to the default sub tree)
+	
+	// TODO: effective algorithm for keeping maximumCapacity
+	// TODO: route path between commands
+	
+}
