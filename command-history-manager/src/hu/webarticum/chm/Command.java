3a900aa60ec77549b1426dfc0c83ea5cd7481c51
horvath <horvath@patronet.net>
2016-12-28 14:27:06 +0100

    Command: docblocks added

--- command-history-manager/src/hu/webarticum/chm/Command.java
+++ command-history-manager/src/hu/webarticum/chm/Command.java
@@ -2,14 +2,40 @@ package hu.webarticum.chm;
 
 import java.util.Date;
 
+/**
+ * Undoable operation.
+ * 
+ * @author Dávid Horváth
+ */
+
 public interface Command {
 	
+	/**
+	 * Executes this command
+	 * 
+	 * @return {@code true} if command was successfully executed
+	 */
 	public boolean execute();
-	
+
+	/**
+	 * Rolls back this command
+	 * 
+	 * @return {@code true} if command was successfully rolled back
+	 */
 	public boolean rollBack();
-	
+
+	/**
+	 * Returns {@code true} if command have been already executed and was not rolled back.
+	 * 
+	 * @return {@code true} if command have been already executed
+	 */
 	public boolean isExecuted();
 	
+	/**
+	 * Returns date when this command was first executed.
+	 * 
+	 * @return date when this command was first executed or null
+	 */
 	public Date getTime();
 	
 }
